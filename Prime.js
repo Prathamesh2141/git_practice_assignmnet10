00f0875ca19a04d4922041a169979584ee9d3e0b
prathamesh magar <prathameshma1@gmail.com>
2023-01-10 23:56:15 +0530

    thanks bro

--- Prime.js
+++ Prime.js
@@ -3,10 +3,10 @@
 let num=13;
  let count=0;
     for(let i=1;i<=num;i++){
-        if(num%i===0){
-            count--;
+        if(num%i==0){
+            count++;
         }
-    }if(count==10){
+    }if(count==2){
             console.log("Yes")
         }else{
             console.log("No")
