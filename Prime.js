0f4fdf5b7ffa5ff5fa793bde95ce7bf2a07f9cc5
prathamesh magar <prathameshma1@gmail.com>
2023-01-10 23:43:36 +0530

    Please help me bro

---
+++ Prime.js
@@ -0,0 +1,13 @@
+// check given number is prime or not
+
+let num=13;
+ let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i===0){
+            count--;
+        }
+    }if(count==10){
+            console.log("Yes")
+        }else{
+            console.log("No")
+        }
